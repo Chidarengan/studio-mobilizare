e1872d54c87214d7079fb62a2fe6450e4fce0220
Chidarengan <lucasdesamartins@hotmail.com>
2026-07-06 00:56:13 -0400

    feat: configuração inicial do projeto

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="text-center p-10">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Studio Mobilizare
+      </h1>
+      <p className="mt-4 text-lg">Site em construção...</p>
+    </div>
+  )
+}
+
+export default App;
